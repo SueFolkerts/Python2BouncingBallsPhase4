c8e7444c76778ee1ebb05586d9ce00ec16504abd
Sue Folkerts <sfolkerts@coinstar.com>
2018-07-17 11:07:57 -0700

    trying

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+import sys
+
+import pygame
+from pygame.locals import *
+
 from ball import *
 balls = []
 
 def main():
+    pygame.init()
+    clock = pygame.time.Clock()
+    color = (26, 255, 255)
+    screen_info = pygame.display.Info()
+    # set the width and height to the size of the screen
+    size = (width, height) = (screen_info.current_w - 20, screen_info.current_h - 20)
+    screen = pygame.display.set_mode(size)
     for i in range(10):
         balls.append(Ball((width / 2, height / 2)))
 
